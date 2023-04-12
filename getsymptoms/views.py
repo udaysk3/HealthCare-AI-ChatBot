77be9d3e07457affeb1236494c5435bcc2d6c5ce
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-04-12 18:48:02 +0530

    Add existing project files to Git

--- getsymptoms/views.py
+++ getsymptoms/views.py
@@ -1,31 +1,3 @@
 from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
-def getsymptoms(req):
-    if(req.method=='POST'):
-        print(req)
-        # i = req.POST.get('queryResult')
-        # par = i['parameters']
-        # for k,v in par.items():
-        #   if k=='person':
-        #     person = par[k]['name']
-        #   elif k=='Feeling':
-        #     Feeling = par['Feeling']
-        #   elif k=='Symptoms':
-        #     Symptoms = par['Symptoms']
-        # print(Feeling, person, Symptoms)
-        return JsonResponse({
-  "fulfillmentMessages": [
-    {
-      "text": {
-        "text": [
-          "Don't worry bro here are the steps you need to take"
-        ]
-      }
-    }
-  ]
-})
-    return HttpResponse('Hello')
-        
+# Create your views here.
