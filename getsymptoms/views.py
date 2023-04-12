619519e88e28436e90f30bbb7a82b3e3fb573b08
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-04-12 18:51:30 +0530

    Add existing project files to Git

--- getsymptoms/views.py
+++ getsymptoms/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse,JsonResponse
 
 # Create your views here.
+
+def getsymptoms(req):
+    if req=='POST':
+        return JsonResponse(
+            {
+                    "fulfillmentMessages": [
+                        {
+                        "text": {
+                            "text": [
+                            "Text response from webhook"
+                            ]
+                        }
+                        }
+                    ]
+            }   
+        )
