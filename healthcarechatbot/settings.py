263cba11c08a3e17bed826222a2396a589742fca
udaysk3 <burluudaysantoshkumar3@gmail.com>
2023-05-20 09:21:15 +0530

    Made cahnges to bot

--- healthcarechatbot/settings.py
+++ healthcarechatbot/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 # Make a tuple of strings instead of a string
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR , "static")
-# ]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
